083307fa01eb6b603f4abb79531816a3feda212a
rishu097 <108551580+rishu097@users.noreply.github.com>
2023-06-22 00:22:17 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 app.use(cors({
-    origin: ["http://localhost:3000","https://mern-task-rishu-app.onrender.com"]
+    origin: ["http://localhost:3000","https://mern-task-rishu-frontend-app.onrender.com"]
 }))
 app.use("/api/tasks",taskRoutes);
 
